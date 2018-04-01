aa995f2643d8c95b782d3df88a8dce8357acf5fb
Ponchai Reainthong <aadenco@gmail.com>
2018-04-01 15:20:35 -0500

    Adding  NG Dropdown component but has issues. Not working.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { BrowserModule } from '@angular/platform-browser';
+import {NgbModule, NgbDropdown} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { MyNewComponentComponent } from './my-new-component/my-new-component.component';
 import { NewCmpComponent } from './new-cmp/new-cmp.component';
+import { NgbdDropdownBasic } from './dropdown-basic';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MyNewComponentComponent,
-    NewCmpComponent
+    NewCmpComponent,
+    NgbdDropdownBasic
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
