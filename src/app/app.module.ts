47f127e07c655c81121a359ffeb53dad92f8603f
Ponchai Reainthong <aadenco@gmail.com>
2018-04-07 18:55:40 -0500

    added top nav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import {NgbModule, NgbDropdown} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 
 import { NgbdDropdownBasic } from './dropdown-basic';
+import { TopnavComponent } from './components/topnav/topnav.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NgbdDropdownBasic
+    NgbdDropdownBasic,
+    TopnavComponent
   ],
   imports: [
     BrowserModule,
