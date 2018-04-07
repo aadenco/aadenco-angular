3bde298c38d71fe339d753b0772f2763f880d55e
Ponchai Reainthong <aadenco@gmail.com>
2018-04-07 13:26:42 -0500

    app structure cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import {NgbModule, NgbDropdown} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
-import { MyNewComponentComponent } from './my-new-component/my-new-component.component';
-import { NewCmpComponent } from './new-cmp/new-cmp.component';
+
 import { NgbdDropdownBasic } from './dropdown-basic';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyNewComponentComponent,
-    NewCmpComponent,
     NgbdDropdownBasic
   ],
   imports: [
