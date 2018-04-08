8d6b88ff8b9ca3fc4743576c4a2f8747b47439ae
Ponchai Reainthong <aadenco@gmail.com>
2018-04-08 12:40:59 -0500

    isCollapsed

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   title = 'Angular and NG-Bootstrap';
+  public isCollapsed = false;
 
   constructor() { }
 
